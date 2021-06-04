f9e453ba5d27aae1ccd92f8cc5c03a30b69d33e3
kcenceis <25601736a@gmail.com>
2021-06-04 11:11:29 +0800

    fix bug

--- dl.py
+++ dl.py
@@ -13,7 +13,8 @@ def download(ex_info):
         os.mkdir(path)
     img_format = re.findall('\.(jpg|bmp|png|jpeg|webp|gif)', ex_info.preview_address)[0]
     r = Utils.getRequest(ex_info.preview_address)
-    with open(path + ex_info.file_name + '.' + img_format, 'wb') as file:
+    ex_info.file_name = ex_info.file_name + '.' + img_format
+    with open(path + ex_info.file_name, 'wb') as file:
         file.write(r.content)
         file.close()
         SQLUTILS.insertSQL(ex_info)
